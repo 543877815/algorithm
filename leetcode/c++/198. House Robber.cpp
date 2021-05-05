044da2169b3b33f2868ce7dcdeb1e4ad5b17214b
李逢君 <543877815@qq.com>
2021-05-05 15:54:53 +0800

    740

--- leetcode/c++/198. House Robber.cpp
+++ leetcode/c++/198. House Robber.cpp
@@ -17,3 +17,39 @@ int rob(int *nums, int numsSize) {
     return F[numsSize - 1];
 }
 
+// 空间优化
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+class Solution {
+public:
+    int rob(vector<int> &nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        int first = nums[0], second = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            int tmp = second;
+            second = max(first + nums[i], second);
+            first = tmp;
+        }
+        return second;
+    }
+};
+
+// 空间复杂度：O(n)
+// 时间复杂度：O(n)
+class Solution {
+public:
+    int rob(vector<int> &nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+};

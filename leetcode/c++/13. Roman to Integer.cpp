7f715c7e94addabf00d8b9538cb4845879e3e1a9
543877815 <543877815@qq.com>
2021-05-15 00:18:30 +0800

    13

--- leetcode/c++/13. Roman to Integer.cpp
+++ leetcode/c++/13. Roman to Integer.cpp
@@ -28,3 +28,32 @@ int romanToInt(char * s){
     ans += nums[indexOf(romanChar, s[length - 1])];
     return ans;
 }
+
+
+class Solution {
+public:
+    unordered_map<char, int> symbol2value = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000},
+    };
+    int romanToInt(string s) {
+        int res = 0;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            int value = symbol2value[s[i]];
+            if (i < n - 1 && value < symbol2value[s[i+1]]) {
+                res -= value;
+            } else {
+                res += value;
+            }
+        }
+
+        return res;
+    }
+};

9612b915279469544de80893319f78ed83022797
543877815 <543877815@qq.com>
2021-05-21 11:54:40 +0800

    1035

---
+++ leetcode/c++/1035. Uncrossed Lines.cpp
@@ -0,0 +1,22 @@
+// 动态规划
+// 时间复杂度：O(nm)
+// 空间复杂度：O(nm)
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        // dp[i][j] 表示到 A[i] 到 B[j] 的最多直线相交
+        vector<vector<int>>dp (n + 1, vector<int>(m + 1, 0));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};

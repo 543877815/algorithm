91698694ba53b7f5ce7bd52eb4c7a22ec347c67d
543877815 <543877815@qq.com>
2021-08-18 15:27:34 +0800

    update 20210818

--- main.cpp
+++ main.cpp
@@ -22,52 +22,27 @@ struct TreeNode {
 
 class Solution {
 public:
-    int minDays(vector<int> &bloomDay, int m, int k) {
-        int n = bloomDay.size();
-        if (n < m * k) return -1;
-        set<int> st(bloomDay.begin(), bloomDay.end()); // set 去重
-        vector<int> vt(st.begin(), st.end()); // 方便遍历
-        int left = 0, right = vt.size() - 1;
-        int res = INT_MAX;
-        while (left <= right) {
-            int middle = (right + left) / 2;
-            int day = vt[middle];
-            int count = 0, bunch = 0;
-            for (int i = 0; i < n; i++) {
-                if (day >= bloomDay[i]) {
-                    count++;
-                } else {
-                    count = 0;
-                }
-                if (count >= k && count % k == 0) {
-                    bunch++;
-                    if (bunch == m) {
-                        res = min(res, day);
-                    }
-                }
+    vector<int> countBits(int n) {
+        vector<int> res = {0};
+        int index = 1;
+        while (index <= n) {
+            int m = res.size();
+            for (int i = 0; i < m && res.size() <= n; i++) {
+                res.push_back(res[i] + 1);
             }
-            if (day == res) right = middle - 1;
-            else left = middle + 1;
+            index *= 2;
         }
-        return res == INT_MAX ? -1 : res;
+
+        return res;
     }
 };
 
 int main() {
     auto *solution = new Solution();
-    TreeNode *one = new TreeNode(1);
-    TreeNode *two = new TreeNode(2);
-    TreeNode *three = new TreeNode(3);
-    TreeNode *four = new TreeNode(4);
-    TreeNode *five = new TreeNode(5);
-    TreeNode *six = new TreeNode(6);
+    TreeNode *five1 = new TreeNode(5);
+    TreeNode *eight = new TreeNode(8);
+    TreeNode *five2 = new TreeNode(5);
 
-    one->right = two;
-    one->left = three;
-    two->right = six;
-    three->right = four;
-    three->left = five;
-
-    vector<int> bloomDay = {1, 10, 3, 10, 2};
-    solution->minDays(bloomDay, 3, 1);
+    vector<int> a = {1, 2, 3, 4};
+    solution->countBits(2);
 }

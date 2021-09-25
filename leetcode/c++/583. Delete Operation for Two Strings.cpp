cca709091aad7b2f9471eecf20d257fced110433
543877815 <543877815@qq.com>
2021-09-25 22:20:42 +0800

    583

---
+++ leetcode/c++/583. Delete Operation for Two Strings.cpp
@@ -0,0 +1,22 @@
+// 动态规划 - 1143最长公共子序列
+// 时间复杂度：O(nm)
+// 空间复杂度：O(n+m)
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector <vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return m + n - 2 * dp[m][n];
+    }
+};
